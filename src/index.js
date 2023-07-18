36759e6d22f7b72eb2a5517cc40cf428e3056ac1
andika98 <121611042@mhs.uqgresik.ac.id>
2023-07-18 15:26:13 +0700

    delete and put

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Selamat Datang di API ')
 })
 
 
@@ -36,4 +36,50 @@ app.post('/products', async (req, res) => {
         messsage: "Data berhasil ditambahkan!!"
     })
 })
+
+app.delete('/products/:id', async(req, res)=>{
+    const productId = req.params.id
+
+    await prisma.product.delete({
+        where: {
+            id : Number(productId),
+        }
+    })
+    res.send("product deleted")
+})
+
+
+app.put('/products/:id', async(req, res) =>{
+    const productId = req.params.id
+  
+    const productData = req.body
+    const existingProduct = await prisma.product.findUnique({
+        where:{
+            id:Number(productId)
+        }
+    })
+    if(!existingProduct){
+        res.status(404).send("Data ditak ditemukan!!")
+    }else{
+         const product = await prisma.product.update({
+        where : {
+            id: Number(productId)
+        },
+        data: {
+                name: productData.name,
+                description: productData.description,
+                price: productData.price,
+                image: productData.image
+            }
+        })
+
+        res.send({
+            data: product,
+            messsage : "Edit Product Success"
+        })
+    }
+   
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
